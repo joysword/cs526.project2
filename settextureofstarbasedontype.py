25cd2dbf946d8ceb0ca438eeefd2c1baa44d69c1
Shi Yin <joysword@me.com>
2013-10-18 17:10:25 -0500

    FIX: last commit wrong

--- settextureofstarbasedontype.py
+++ settextureofstarbasedontype.py
@@ -9,23 +9,24 @@ atLine = 0
 for line in csvread:
 	atLine += 1
 	if atLine==1:
+		csvwrite.writerow(line)
 		continue
 	print 'at line:',atLine
 	if int(line[1])==1:
 		if cmp(line[8],'O')==0:
-			line[3]='textures/o.png'
+			line[4]='textures/o.png'
 		elif cmp(line[8],'B')==0:
-			line[3]='textures/b.png'
+			line[4]='textures/b.png'
 		elif cmp(line[8],'A')==0:
-			line[3]='textures/a.jpg'
+			line[4]='textures/a.jpg'
 		elif cmp(line[8],'F')==0:
-			line[3]='textures/f.png'
+			line[4]='textures/f.png'
 		elif cmp(line[8],'G')==0:
-			line[3]='textures/g.png'
+			line[4]='textures/g.png'
 		elif cmp(line[8],'K')==0:
-			line[3]='textures/k.png'
+			line[4]='textures/k.png'
 		elif cmp(line[8],'M')==0:
-			line[3]='textures/m.png'
+			line[4]='textures/m.png'
 
 	csvwrite.writerow(line)
 f.close()
